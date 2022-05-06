19783d8ee9f1fccd68a2de4b4c08e78f68d06711
Pedro <pedroscardovelli@gmail.com>
2022-05-06 20:08:10 -0300

    finaliza

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int numeroDigitado;
+
+Console.Write("Digite um número: ");
+numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+{
+if (numeroDigitado < 0)
+{
+Console.WriteLine("Você digitou um número negativo");
+}
+}
